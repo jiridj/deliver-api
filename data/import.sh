303b934445d5ee3b3d8dd9ef54990a8542f80d9a
Jiri De Jagere <jiri.dejagere@softwareag.com>
2023-03-01 14:29:33 +0100

    fix mongo export import

--- data/import.sh
+++ data/import.sh
@@ -1,3 +1,3 @@
-mongoimport --uri mongodb://deliver-api:s3cr3t@mongo-db:27017/deliver --collection products --drop --file /data/export/products 
-mongoimport --uri mongodb://deliver-api:s3cr3t@mongo-db:27017/deliver --collection users --drop --file /data/export/users
-mongoimport --uri mongodb://deliver-api:s3cr3t@mongo-db:27017/deliver --collection orders --drop --file /data/export/orders
+mongoimport --uri mongodb://deliver-api:s3cr3t@mongo-db:27017/deliver --collection products --drop --file /data/export/products.json
+mongoimport --uri mongodb://deliver-api:s3cr3t@mongo-db:27017/deliver --collection users --drop --file /data/export/users.json
+mongoimport --uri mongodb://deliver-api:s3cr3t@mongo-db:27017/deliver --collection orders --drop --file /data/export/orders.json
